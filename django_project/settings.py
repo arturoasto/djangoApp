a614b625ebeec6f5b00b735120d2928f1e3019bc
arturo asto <arturo.asto@hotmail.com>
2020-01-12 19:19:17 -0500

    changing secret key and debug

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-&pt^^(v^xqtti(!ma&0-fj+v*igj0%s&4or($d)zh+h(qjand'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_VALUE')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['catblogapp.herokuapp.com']
 
 
 # Application definition
